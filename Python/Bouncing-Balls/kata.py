e157c288513b009225b1e43e24cf8287e1ab19e3
Adrian Stankiewicz <AdrianStankiewiczIT@gmail.com>
2023-05-24 15:46:54 +0200

    added some of already completed Python katas

--- Python/Bouncing-Balls/kata.py
+++ Python/Bouncing-Balls/kata.py
@@ -1,11 +1,7 @@
+import math
+
 def bouncing_ball(h, bounce, window):
-    if h <= 0 or window >= h or bounce < 0 or bounce > 1:
+    if h <= 0 or window >= h or bounce <= 0 or bounce >= 1:
         return -1
     
-    times_seen = 1
-    
-    while h*bounce > window:
-        times_seen += 2
-        h *= bounce
-        
-    return times_seen
+    return 1 + 2*int(h*bounce > window) * math.floor(math.log(window/h, bounce))
